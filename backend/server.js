4f6f19255a932e75f14c55f5f130350e83a4bd83
developertarun01 <tarunbusinessmail@gmail.com>
2025-12-08 14:21:49 +0530

    weyfar url

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,8 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     'http://localhost:5173',
-    'https://farematchpro-flight.vercel.app',
-    'https://farematchpro.online'
+    'https://weyfar-flight.vercel.app',
+    'https://weyfar.com'
   ],
   credentials: true,
   optionsSuccessStatus: 200
